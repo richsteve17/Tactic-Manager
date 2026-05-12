2f7df9fdcfb1aec9a722b0b712a8017b9e42a934
Steve Coleman <80608382+richsteve17@users.noreply.github.com>
2026-05-12 01:46:36 -0400

    Configure Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/Tactic-Manager/' : '/',
   plugins: [react()],
   test: {
     globals: true,
